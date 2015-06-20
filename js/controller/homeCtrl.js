47002f9ae9da5998996933b56cd0d7060a282d0c
Kévin <k.sourrue@gmail.com>
2015-06-20 23:01:09 +0200

    Ajout loader

--- js/controller/homeCtrl.js
+++ js/controller/homeCtrl.js
@@ -5,4 +5,17 @@ app.controller("homeCtrl", function($scope){
     // http://api.openweathermap.org/data/2.5/find?q=bordeaux&units=metric&lang=fr&type=accurate
     // http://openweathermap.org/weather-conditions
     
+    // Initialisation du loader
+    $scope.goSearch = false;
+    
+    // Fonction recherche ville
+    $scope.search = function(){
+        
+        // Lancement du loader
+        $scope.goSearch = true;
+        
+        // Lancement de la recherche = requête api
+        
+    };
+    
 });
--- views/home.php
+++ views/home.php
@@ -2,11 +2,15 @@
 
 <!--First block-->
 <div id="block1" class="container-fluid block">
-    <form>
+    <form name="searchCity">
         <label for="city">Recherchez une ville afin d&rsquo;afficher la m&eacute;t&eacute;o</label>
         <input type="search" placeholder="Ville" class="form-control" name="city" />
-        <button class="btn btnGreen">Rechercher</button>
+        <button class="btn btnGreen" ng-click="search()" ng-class="{none: goSearch == true}">Rechercher</button>
     </form>
+    <div id="loader" ng-class="{none: goSearch == false}">
+        <span>Recherche en cours</span>
+        <span><i class="glyphicon glyphicon-refresh"></i></span>
+    </div>
 </div>
 
 <!--Second block-->
